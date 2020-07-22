3779afd01f5ab6b82b6335e15118a3593d6c1c60
Diego Sousa <diegors90@gmail.com>
2020-07-21 22:57:19 -0300

    add tests to ProdutoController

--- src/test/java/com/diego/pedidosspring/controller/ProdutoControllerTest.java
+++ src/test/java/com/diego/pedidosspring/controller/ProdutoControllerTest.java
@@ -2,20 +2,22 @@ package com.diego.pedidosspring.controller;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,68 +25,71 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.diego.pedidosspring.PedidosSpringApplication;
-import com.diego.pedidosspring.controllers.CategoriaController;
+import com.diego.pedidosspring.controllers.ProdutoController;
 import com.diego.pedidosspring.controllers.exceptions.ControllerExceptionHandler;
 import com.diego.pedidosspring.model.Categoria;
 import com.diego.pedidosspring.model.Produto;
 import com.diego.pedidosspring.security.JWTUtil;
 import com.diego.pedidosspring.services.CategoriaService;
+import com.diego.pedidosspring.services.ProdutoService;
 import com.diego.pedidosspring.services.exceptions.ObjectNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.net.HttpHeaders;
 
 @WithMockUser(username = "admin")
-@WebMvcTest(CategoriaController.class)
+@WebMvcTest(ProdutoController.class)
 @ContextConfiguration(classes = {
 		PedidosSpringApplication.class,
 		JWTUtil.class
 })
-public class CategoriaControllerTest {
+public class ProdutoControllerTest {
 
-	private final String BASE_URL = "/categorias";
+	private final String BASE_URL = "/produtos";
 	private ObjectMapper objectMapper;
+	private Produto produto;
+	private Categoria categoria;
+	private List<Produto> produtos;
 	@Autowired
 	private MockMvc mockMvc;
-	
 	@Autowired
-	private CategoriaController categoriaController;
-	
+	private ProdutoController produtoController;
+	@MockBean
+	private ProdutoService produtoService;
 	@MockBean
 	private CategoriaService categoriaService;
 	
 	@BeforeEach
 	public void setUp() {
 		objectMapper = new ObjectMapper();
-		mockMvc = MockMvcBuilders.standaloneSetup(categoriaController)
+		categoria = new Categoria(1L, "Esporte");
+		produto = new Produto("001", "Camisa", 90.00, categoria);
+		produtos = Arrays.asList(produto);
+		mockMvc = MockMvcBuilders.standaloneSetup(produtoController)
 				.setControllerAdvice(new ControllerExceptionHandler()).build();
 	}
 	
-	
 	@Test
 	public void itShouldReturnOk_WhenFindById() throws Exception {
-		when(this.categoriaService.findById(1L)).thenReturn(new Categoria(1L, "Esporte"));
+		when(this.produtoService.findById(1L)).thenReturn(produto);
 		
 		mockMvc.perform(get(BASE_URL + "/1")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.descricao", is("Esporte")));
+				.andExpect(jsonPath("$.descricao", is("Camisa")));
 	}
 	
 	@Test
 	public void itShouldReturnNotFound_WhenFindById() throws Exception {
-		when(this.categoriaService.findById(2L)).thenThrow(new ObjectNotFoundException("Not Found!"));
+		when(this.produtoService.findById(2L)).thenThrow(new ObjectNotFoundException("Not Found!"));
 		
 		mockMvc.perform(get("/2")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound());
 	}
 	
-	@WithMockUser(username = "admin")
 	@Test
 	public void itShouldReturnOk_WhenFindAll() throws Exception {
 		
-		when(this.categoriaService.findAll()).thenReturn(
-				Arrays.asList(new Categoria(1L, "Esporte"), new Categoria(2L, "Futebol")));
+		when(this.produtoService.findAll()).thenReturn(produtos);
 		
 		mockMvc.perform(get(BASE_URL)
 				.contentType(MediaType.APPLICATION_JSON))
@@ -94,10 +99,9 @@ public class CategoriaControllerTest {
 	@Test
 	public void itShouldReturnOk_WhenFyndByDescricao() throws Exception {
 		
-		when(this.categoriaService.findByDescricaoContainingIgnoreCase("Esporte"))
-			.thenReturn(Arrays.asList(new Categoria(1L, "Esporte")));
+		when(this.produtoService.findByDescricaoContainsIgnoreCaseOrCodigo("Camisa", null)).thenReturn(produtos);
 		
-		mockMvc.perform(get(BASE_URL + "/search/Esporte")
+		mockMvc.perform(get(BASE_URL + "/search/Camisa")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
@@ -106,18 +110,9 @@ public class CategoriaControllerTest {
 	public void itShouldReturnCreated_WhenSave() throws Exception {
 		
 		mockMvc.perform(post(BASE_URL)
-				.content(objectMapper.writeValueAsString(new Categoria(1L, "Esporte")))
+				.content(objectMapper.writeValueAsString(produto))
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
 				.andExpect(status().isCreated());
-		
-	}
-	
-	@Test
-	public void itShouldReturnBadRequest_WhenSave() throws Exception {
-		mockMvc.perform(post(BASE_URL)
-				.content(objectMapper.writeValueAsString(new Categoria()))
-				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-				.andExpect(status().isBadRequest());
 		
 	}
 	
@@ -125,15 +120,14 @@ public class CategoriaControllerTest {
 	public void itShouldReturnOk_WhenUpdate() throws Exception {
 		
 		mockMvc.perform(put(BASE_URL)
-				.content(objectMapper.writeValueAsString(new Categoria(1L, "New name")))
+				.content(objectMapper.writeValueAsString(new Produto(1L, "001", "New name", 40.00, categoria)))
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
 	
 	@Test
 	public void itShouldReturnNoContent_WhenDelete() throws Exception {		
-		mockMvc.perform(delete(BASE_URL + "/1"))
+		mockMvc.perform(delete(BASE_URL + "/5"))
 				.andExpect(status().isNoContent());
 	}
-	
 }
