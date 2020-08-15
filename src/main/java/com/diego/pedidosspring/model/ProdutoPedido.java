e90f43117d7a674db00e2447b93c271603d0c45b
Diego Sousa <diegors90@gmail.com>
2020-08-15 00:01:08 -0300

    add model to Pedido

---
+++ src/main/java/com/diego/pedidosspring/model/ProdutoPedido.java
@@ -0,0 +1,19 @@
+package com.diego.pedidosspring.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Digits;
+
+public class ProdutoPedido {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Digits(integer = 5, fraction = 2, message="Apenas números com duas casas decimais")
+	private String quantidade;
+	@Digits(integer = 5, fraction = 2, message="Apenas números com duas casas decimais")
+	private Double preco;
+	@ManyToOne
+	private Produto produto;
+}
